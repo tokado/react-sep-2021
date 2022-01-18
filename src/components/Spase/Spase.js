6a2fa819df3238be242c4fd4a99c7b98ddc1e641
Kio Tokado <ole.chaykovskiy@gmail.com>
2022-01-18 16:24:55 +0200

    Changes

--- src/components/Spase/Spase.js
+++ src/components/Spase/Spase.js
@@ -3,10 +3,10 @@ import "./index.css"
 const Spase = (props) => {
     const {mission_name,launch_year,mission_patch_small} = props;
     return (
-        <div className={"main"}>
+        <div className={"main__2"}>
             <div>mission_name: {mission_name}</div>
             <div>launch_year: {launch_year}</div>
-            <img className={"img"} src={mission_patch_small} alt={mission_name}/>
+           <div className={'main__img_div'}><img className={"img__main"} src={mission_patch_small} alt={mission_name}/></div>
         </div>
     );
 };
--- src/components/Spases/Spases.js
+++ src/components/Spases/Spases.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import "../Spase/index.css"
 
 const Spases = () => {
     const [spases,setSpases] = useState([])
